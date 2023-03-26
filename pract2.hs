5f1fafb24ea061ade62470b885d4f957e3b17ecd
SrkBlid <mezzanojoaquin@gmail.com>
2023-03-26 16:01:29 -0300

    Ejercicios del 3 al 7 (Menos el 4)

--- pract2.hs
+++ pract2.hs
@@ -19,3 +19,30 @@ init2 :: [a] -> [a]
 init2 [x] = []
 init2 (x:xs) = x : init2 xs
 
+--3
+maxTres :: Int -> Int -> Int -> Int
+maxTres x y z
+        | (x >= y) && (x >= z) = x
+        | (y >= x) && (y >= z) = y
+        | (z >= x) && (z >= y) = z
+
+--4
+
+--5
+abs2 :: Int -> Int
+abs2 x
+    | x < 0 = x*(-1)
+    | x >= 0 = x
+
+--6
+edad :: (Int,Int,Int) -> (Int,Int,Int) -> Int
+edad (a,b,c) (d,e,f)
+        | c >= f = c-f
+        | c < f = f-c
+
+--7
+xor :: Bool -> Bool -> Bool
+xor x y
+    | x && not y = True
+    | not x && y = True
+    | otherwise = False
