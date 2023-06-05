04bcd3a8cecfb4380c1620899fe6b5aa2c3d8dac
SrkBlid <mezzanojoaquin@gmail.com>
2023-06-04 22:05:00 -0300

    Prácticado hasta ternas de Hoarde, ej 4 sin terminar

--- pract2doP.hs
+++ pract2doP.hs
@@ -489,3 +489,460 @@ x = y ∧ <∃bs :: ys = xs++bs> v <∃as, bs :: ys = as++(x:xs)++bs>
 Q.[].ys = True
 Q.(x:xs).ys = False
 Q.(x:xs).(y:ys) = (x = y) v Q.xs.ys
+
+------------------------------------------------------------------------------
+--EJERCICIO 10: Especificar y derivar una función que dada una lista de números
+-- calcula el promedio de la misma, recorriendo la lista una sola vez (tupling)
+suma.xs = <∑i : 0 <= i < #xs : xs.i>
+cant.xs = <Ni : 0 <= i < #xs : 1>
+f.xs = (suma.xs, cant.xs)
+
+--DERIVACIÓN SUMA
+--CB xs = []
+suma.[]
+    [ESPECIFICACIÓN]
+<∑i : 0 <= i < #[] : [].i>
+    [DEF CARDINAL]
+<∑i : 0 <= i < 0 : [].i>
+    [RANGO VACIO]
+0
+
+--CI xs = x:xs
+--HI suma.xs = <∑i : 0 <(a, b) = f.xs= i < #xs : xs.i>
+suma.(x:xs)
+    [ESPECIFICACIÓN]
+<∑i : 0 <= i < #x:xs : (x:xs).i>
+    [DEF CARDINAL]
+<∑i : 0 <= i < 1+#xs : (x:xs).i>
+    [LÓGICA, PARTICIÓN DE RANGO]
+<∑i : 0 <= i < 1 : (x:xs).i> + <∑i : 1 <= i < 1+#xs : (x:xs).i>
+    [RANGO UNITARIO]
+(x.xs).0 + <∑i : 1 <= i < 1+#xs : (x:xs).i>
+    [ARITMETICA]
+x + <∑i : 1 <= i < 1+#xs : (x:xs).i>
+    [i <- i+1, ARITMETICA, DEF POSICIÓN]
+x + <∑i : 0 <= i < #xs : xs.i>
+    [HI]
+x + suma.xs
+
+--DERIVACIÓN CANT
+--CB xs = []
+cant.[]
+    [ESPECIFICACIÓN]
+<Ni : 0 <= i < #[] : 1>
+    [DEF CARDINAL]
+<Ni : 0 <= i < 0 : 1>
+    [RANGO VACIO]
+0
+
+--CI xs = x:xs
+--HI cant.xs = <Ni : 0 <= i < #xs : 1>
+cant.(x:xs)
+    [ESPECIFICACIÓN]
+<Ni : 0 <= i < #x:xs : 1>
+    [DEF CARDINAL]
+<Ni : 0 <= i < 1+#xs : 1>
+    [LÓGICA, PARTICIÓN DE RANGO]
+<Ni : 0 <= i < 1 : 1> + <Ni : 1 <= i < 1+#xs : 1>
+    [RANGO UNITARIO]
+1 + <Ni : 1 <= i < 1+#xs : 1>
+    [i <- i+1, ARITMETICA]
+1 + <Ni : 0 <= i < #xs : 1>
+    [HI]
+1 + cant.xs
+
+suma.[] = 0
+suma.(x:xs) = x + suma.xs
+
+cant.[] = 0
+cant.(x:xs) = 1 + cant.xs
+
+--DERIVAMOS AHORA F
+f.xs = (suma.xs, cant.xs)
+
+--CB xs = []
+f.[]
+    [ESPECIFICACIÓN]
+(suma.[], cant.[])
+    [ESPECIFICACIONES SUMA Y CANT]
+(0, 0)
+
+--CI xs = x:xs
+--HI f.xs = (suma.xs, cant.xs)
+f.(x:xs)
+    [ESPECIFICACIÓN]
+(suma.(x:xs), cant.(x:xs))
+    [ESPECIFICACIONES SUMA Y CANT]
+(x+suma.xs, 1+cant.xs)
+    [INTRODUCCIÓN -> a = suma.xs, b = cant.xs]
+(x+a, 1+b)
+    [IGUALDAD DE PARES -> (a, b) = (suma.xs, cant.xs)]
+(x+a, 1+b)
+    [HI]
+(a, b) = f.xs
+
+--CONCLUSIÓN
+f.[] = (0, 0)
+f.(x:xs) = (x+a, 1+b)
+            [(a,b) = f.xs]
+
+promedio = f.xs.0/f.xs.1
+        f.xs.0 = suma.xs, f.xs.1 = cant.xs
+
+------------------------------------------------------------------------------
+--INDUCCIÓN Y RECURSIÓN, EJERCICIOS DEL LIBRO
+--PÁGINA 169
+------------------------------------------------------------------------------
+--EJERCICIO 1: Dada la sucesión de fibonacci:
+--  fib.0 = 0
+--  fib.1 = 1
+--  fib(n+2) = fib.n + fib.(n+1)
+--
+-- Demostrar la siguiente especificación:
+-- <∀n : 0 <= n : fib.n < 2^n>
+
+--CB n = 0
+fib.0
+    [ESPECIFICACIÓN]
+<∀n : 0 <= 0 : fib.0 < 2^0>
+    [RANGO VACIO]
+True
+
+--CB n = 1
+fib.1
+    [ESPECIFICACIÓN]
+<∀n : 0 <= 1 : fib.1 < 2^1>
+    [RANGO UNITARIO]
+fib.1 < 2^1
+    [DEF FIB, ARITMETICA]
+1 < 2
+    [LÓGICA]
+True
+
+--CI n = n+1
+--HI fib.n = <∀n : 0 <= n : fib.n < 2^n>
+fib.(n+1)
+    [ESPECIFICACIÓN]
+<∀n : 0 <= n+1 : fib.(n+1) < 2^(n+1)>
+    [n <- n+1]
+<∀n : 0 <= (n+1)+1 : fib.((n+1)+1) < 2^((n+1)+1)>
+    [ARITMETICA]
+<∀n : 0 <= n+2 : fib.(n+2) < 2^(n+2)>
+    [DEF FIB, DEF POTENCIA]
+<∀n : 0 <= n+2 : fib.n + fib.(1+1) < 2^n*2^2)>
+    [DEF FIB, ARITMETICA]
+<∀n : 0 <= n+2 : fib.n + fib.1 + fib.1 < 2^n*2^2)>
+    [DEF FIB]
+<∀n : 0 <= n+2 : fib.n + 1 + 1 < 2^n*2^2)>
+    [ARITMETICA]
+<∀n : 0 <= n+2 : 2 + fib.n < 2^n*2^2)>
+
+GENERALIZAMOS
+g.n = <∀n : 0 <= n+i : i + fib.n < 2^i*2^n)>
+
+------------------------------------------------------------------------------
+--DERIVACIÓN DE LISTAS, EJERCICIOS DEL LIBRO
+--PÁGINA 201
+------------------------------------------------------------------------------
+--EJERCICIO 1: Derivar una función recursiva para la función
+-- f : Int -> [Num] -> Bool
+-- la cual determinar si el k-esimo elemento de una lista de números aloja el
+-- mínimo valor de la misma
+
+f : Int -> [Num] -> Bool
+f.k.xs = <∀i : 0 <= i < #xs : xs.k <= xs.i>
+
+--CB xs = []
+f.k.[] 
+    [ESPECIFICACIÓN]
+<∀i : 0 <= i < #[] : [].k <= [].i>
+    [DEF CARDINAL]
+<∀i : 0 <= i < 0 : [].k <= [].i>
+    [RANGO VACIO]
+True
+
+--CI xs = x:xs
+--HI f.k.xs = <∀i : 0 <= i < #xs : xs.k <= xs.i>
+f.k.(x:xs)
+    [ESPECIFICACIÓN]
+<∀i : 0 <= i < #(x:xs) : (x:xs).k <= (x:xs).i>
+    [DEF CARDINAL]
+<∀i : 0 <= i < 1+#xs : (x:xs).k <= (x:xs).i>
+    [LÓGICA, PARTICIÓN DE RANGO]
+<∀i : 0 <= i < 1 : (x:xs).k <= (x:xs).i> ∧ <∀i : 1 <= i < 1+#xs : (x:xs).k <= (x:xs).i>
+    [RANGO UNITARIO]
+(x:xs).k <= (x:xs).0 ∧ <∀i : 1 <= i < 1+#xs : (x:xs).k <= (x:xs).i>
+    [DEF POSICIÓN]
+(x:xs).k <= x ∧ <∀i : 1 <= i < 1+#xs : (x:xs).k <= (x:xs).i>
+    [i <- i+1, ARITMETICA, DEF POSICIÓN]
+(x:xs).k <= x ∧ <∀i : 0 <= i < #xs : (x:xs).k <= xs.i>
+
+MODULARIZACIÓN
+g.k.z.xs = <∀i : 0 <= i < #xs : z.k <= xs.i>
+
+--CB xs = []
+g.k.z.[]
+    [ESPECIFICACIÓN]
+<∀i : 0 <= i < #[] : z.k <= [].i>
+    [DEF CARDINAL]
+<∀i : 0 <= i < 0 : z.k <= [].i>
+    [RANGO VACIO]
+True
+
+--CI xs = x:xs
+--HI g.k.z.xs = <∀i : 0 <= i < #xs : z.k <= xs.i>
+g.k.z.(x:xs)
+    [ESPECIFICACIÓN]
+<∀i : 0 <= i < #(x:xs) : z.k <= (x:xs).i>
+    [DEF CARDINAL]
+<∀i : 0 <= i < 1+#xs : z.k <= (x:xs).i>
+    [LÓGICA, PARTICIÓN DE RANGO]
+<∀i : 0 <= i < 1 : z.k <= (x:xs).i> ∧ <∀i : 1 <= i < 1+#xs : z.k <= (x:xs).i>
+    [RANGO UNITARIO, DEF POSICIÓN, i <- i+1, ARITMETICA]
+z.k <= x ∧ <∀i : 0 <= i < #xs : z.k <= xs.i>
+    [HI]
+z.k <= x ∧ g.k.z.xs
+
+--PARA CONCLUIR
+f.k.[] = True
+f.k.(x:xs) = z.k <= x ∧ g.k.z.xs
+
+------------------------------------------------------------------------------
+--EJERCICIO 2: Derivar una función recursiva para
+-- f.xs.ys = <Min i, j : 0 <= i < #xs ∧ 0 <= j < #ys : |xs.i - ys.j|>
+
+--CB xs = []
+f.[].ys
+    [ESPECIFICACIÓN]
+<Min i, j : 0 <= i < #[] ∧ 0 <= j < #ys : |[].i - ys.j|>
+    [DEF CARDINAL, LÓGICA]
+<Min i, j : False ∧ 0 <= j < #ys : |[].i - ys.j|>
+    [LÓGICA]
+<Min i, j : False : |[].i - ys.j|>
+    [RANGO VACIO]
+-inf
+
+--CB ys = []
+f.[].ys
+    [ESPECIFICACIÓN]
+<Min i, j : 0 <= i < #xs ∧ 0 <= j < #[] : |xs.i - [].j|>
+    [DEF CARDINAL, LÓGICA]
+<Min i, j : 0 <= i < #xs ∧ False : |xs.i - [].j|>
+    [LÓGICA]
+<Min i, j : False : |xs.i - [].j|>
+    [RANGO VACIO]
+-inf
+
+--CI xs = (x:xs), ys = (y:ys)
+--HI f.xs.ys = <Min i, j : 0 <= i < #xs ∧ 0 <= j < #ys : |xs.i - ys.j|>
+<Min i, j : 0 <= i < #(x:xs) ∧ 0 <= j < #(y:ys) : |(x:xs).i - (y:ys).j|>
+    [DEF CARDINAL]
+<Min i, j : 0 <= i < 1+#xs ∧ 0 <= j < 1+#ys : |(x:xs).i - (y:ys).j|>
+    [LÓGICA]
+<Min i, j : ((0 <= i < 1) v (1 <= i < 1+#xs))
+ ∧ ((0 <= j < 1) v (1 <= j < 1+#ys)) : |(x:xs).i - (y:ys).j|>
+    [PARTICIÓN DE RANGO]
+<Min i, j : (0 <= i < 1) ∧ (0 <= j < 1) : |(x:xs).i - (y:ys).j|>
+ 'Min' <Min i, j : (1 <= i < 1+#xs) ∧ (1 <= j < 1+#ys) : |(x:xs).i - (y:ys).j|>
+    [RANGO UNITARIO, ARITMETICA]
+|x - y| 'Min' <Min i, j : (1 <= i < 1+#xs) ∧ (1 <= j < 1+#ys) : |(x:xs).i - (y:ys).j|>
+    [i <- i+1, j <- j+1, ARITMETICA]
+|x - y| 'Min' <Min i, j : 0 <= i < #xs ∧ 0 <= j < #ys : |(x:xs).(i+1) - (y:ys).(j+1)|>
+    [DEF POSICIÓN]
+|x - y| 'Min' <Min i, j : 0 <= i < #xs ∧ 0 <= j < #ys : |xs.i - ys.j|>
+    [HI]
+|x - y| 'Min' f.xs.ys
+
+--PARA CERRAR
+f.[].ys = False
+f.xs.[] = False
+f.(x:xs).(y:ys) = |x - y| 'Min' f.xs.ys
+
+------------------------------------------------------------------------------
+--PRÁCTICO 10
+------------------------------------------------------------------------------
+--EJERCICIO 1: Encontrar la wp
+-- {wp} x := (x-y)*(x+y) {x+y^2 = 0}
+{wp} x := (x-y)*(x+y) {x+y^2 = 0}
+    [DEF WP]
+wp.(x := (x-y)*(x+y)).(x+y^2 = 0)
+    [DEF ASIGNACIÓN WP]
+((x-y)*(x+y))+y^2 = 0
+    [ARITMETICA]
+(x^2+xy-xy-y^2)+y^2 = 0
+    [ARITMETICA]
+x^2-y^2+y^2 = 0
+    [ARITMETICA]
+x^2 = 0
+    [ARITMETICA]
+x = 0
+WEAKEST PRECONDITION -> x = 0
+
+-- {wp}q,r := q+1 , r-y{q*y+r = x}
+{wp}q,r := q+1 , r-y{q*y+r = x}
+    [DEF WP]
+wp.(q,r := q+1, r-y).({q*y+r = x})
+    [DEF ASIGNACIÓN WP]
+(q+1)*y+(r-y) = x
+    [ARITMETICA]
+q*y+y+r-y = x
+    [ARITMETICA]
+q*y+r = x
+WEAKEST PRECONDITION -> q*y-r = x
+
+-- {wp}
+-- a := a = b; (S1)
+-- b := a = b; (S2)
+-- a := a = b; (S3)(b = B) ∧ (a = A)
+    [DEF Q, DEF S3]
+wp.(S1).(wp.(S2).(wp.(a := a = b).((a = B) ∧ (b = A))))
+    [DEF ASIGNACIÓN WP]
+wp.(S1).(wp.(S2).(((a = b = B) ∧ (b = A))))
+    [DEF S2]
+wp.(S1).(wp.(b := a = b).(((a = b = B) ∧ (b = A))))
+    [DEF ASIGNACIÓN WP]
+wp.(S1).((((a = a = b = B) ∧ (a = b = A))))
+    [SIMETRIA]
+wp.(S1).((((b = B) ∧ (a = b = A))))
+    [DEF S1]
+wp.(a := a = b).((((b = B) ∧ (a = b = A))))
+    [DEF ASIGNACIÓN WP]
+((b = B) ∧ (a = b = b = A))
+    [SIMETRIA]
+(b = B) ∧ (a = A)
+WEAKEST PRECONDITION -> (b = B) ∧ (a = A)
+
+------------------------------------------------------------------------------
+--EJERCICIO 2: Calcular expresiones E tal que
+-- {A = q*B+r} q := E; r := r-B {A = q*B+r}
+{A = q*B+r} q := E; r := r-B {A = q*B+r}
+    [DEF WP]
+A = q*B+r -> wp.(q := E; r := r-B).(A = q*B+r)
+    [DEF ASIGNACIÓN WP]
+A = q*B+r -> A = E*B+r-B
+    [LEIBNITZ]
+q*B+r = E*B+r-B
+    [ARITMETICA]
+(q*B+r-r+B)/B = E
+    [ARITMETICA]
+(q*B+B)/B = E
+    [ARITMETICA (FACTOR COMUN B)]
+B*(q+1)/B = E
+    [ARITMETICA]
+E = q+1
+
+--CORROBORAMOS
+A = q*B+r -> A = E*B+r-B
+    [REEMPLAZAMOS E]
+A = q*B+r -> A = (q+1)*B+r-B
+    [ARITMETICA]
+A = q*B+r -> A = q*B+B+r-B
+    [ARITMETICA]
+A = q*B+r -> A = q*B+r
+    [LEIBNITZ]
+A = A
+    [LÓGICA]
+True
+
+-- {x*y+p*q = N} x:= x-p; q := E {x*y+p*q = N}
+{x*y+p*q = N} x:= x-p; q := E {x*y+p*q = N}
+    [DEF WP]
+x*y+p*q = N -> wp.(x:= x-p; q := E).(x*y+p*q = N)
+    [DEF ASIGNACIÓN WP]
+x*y+p*q = N -> (x-p)*y+p*E = N
+    [ARITMETICA]
+x*y+p*q = N -> x*y-p*y+p*E = N
+    [LEIBNITZ]
+x*y-p*y+p*E = x*y+p*q
+    [ARITMETICA]
+E = (x*y+p*q-x*y+p*y)/p
+    [ARITMETICA]
+E = (p*q+p*y)/p
+    [ARITMETICA (FACTOR COMUN P)]
+E = p(q+y)/p
+    [ARITMETICA]
+E = q+y
+
+--CORROBORAMOS
+x*y+p*q = N -> x*y-p*y+p*E = N
+    [REEMPLAZAMOS E]
+x*y+p*q = N -> x*y-p*y+p*(q+y) = N
+    [ARITMETICA]
+x*y+p*q = N -> x*y+p*q = N
+    [LEIBNITZ]
+N = N
+    [LÓGICA]
+True
+
+------------------------------------------------------------------------------
+--EJERCICIO 3: Demostrar la corrección
+-- {x = A ∧ y = B}
+-- x := x-y; (S1)
+-- y := x+y; (S2)
+-- x := y-x; (S3)
+-- {x = B ∧ y = A}
+P -> wp.S1.(wp.S2.(wp.S3.Q))
+    [DEF P, DEF WP S3, DEF Q]
+x = A ∧ y = B -> wp.S1.(wp.S2.(wp.(x := y-x).(x = B ∧ y = A)))
+    [DEF ASIGNACIÓN WP]
+x = A ∧ y = B -> wp.S1.(wp.S2.(y-x = B ∧ y = A))
+    [DEF WP S2]
+x = A ∧ y = B -> wp.S1.(wp.(y := x+y).(y-x = B ∧ y = A))
+    [DEF ASIGNACIÓN WP]
+x = A ∧ y = B -> wp.S1.(x+y-x = B ∧ x+y = A)
+    [DEF WP S1]
+x = A ∧ y = B -> wp.(x := x-y).(x+y-x = B ∧ x+y = A)
+    [ARITMETICA]
+x = A ∧ y = B -> wp.(x := x-y).(y = B ∧ x+y = A)
+    [DEF ASIGNACIÓN WP]
+x = A ∧ y = B -> y = B ∧ x-y+y = A
+    [ARITMETICA]
+x = A ∧ y = B -> y = B ∧ x = A
+    [LEIBNITZ]
+x = A ∧ y = B -> B = B ∧ A = A
+    [LÓGICA (True ∧ True)]
+x = A ∧ y = B -> True
+    [LÓGICA]
+True
+-- Por lo tanto se demuestra la corrección
+
+------------------------------------------------------------------------------
+--EJERCICIO 4: Demostrar la corrección
+-- En todos los casos tenemos (x, y : Int) y (a, b : Bool)
+-- {True}
+-- if x >= y -> skip            (S1)
+-- [] x <= y -> x, y := y, x    (S2)
+-- fi
+-- {x >= y}
+
+-- I. [True -> x >= y v x <= y]
+-- II. {True ∧ x >= y} skip {x >= y}
+-- III. {True ∧ x <= y} x,y := y,x {x >= y}
+
+True -> x >= y v x <= y
+    [LÓGICA]
+True -> True
+    [LÓGICA]
+True
+
+{True ∧ x >= y} skip {x >= y}
+    [DEF WP]
+True ∧ x >= y -> wp.skip.(x >= y)
+    [NEUTRO ∧]
+x >= y -> wp.skip.(x >= y)
+    [DEF WP SKIP]
+x >= y -> x >= y
+    [LÓGICA]
+True
+
+{True ∧ x <= y} x,y := y,x {x >= y}
+    [DEF WP]
+True ∧ x <= y -> wp.(x,y := y,x).(x >= y)
+    [NEUTRO ∧]
+x <= y -> wp.(x,y := y,x).(x >= y)
+    [DEF ASIGNACIÓN WP]
+x <= y -> y >= x
+    [LÓGICA]
+x <= y -> x <= y
+    [LÓGICA]
+True
